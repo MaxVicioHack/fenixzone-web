fc0e4d61e4ccd13f61227ae6988d47bf7bfa1b98
MaxVicioHack <maxvixioturbo@gmail.com>
2025-05-28 01:34:19 -0300

    Update firebase-init.js
    s

--- firebase-init.js
+++ firebase-init.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AQUI_TU_API_KEY",
-  authDomain: "AQUI_TU_AUTH_DOMAIN",
-  projectId: "AQUI_TU_PROJECT_ID",
-  storageBucket: "AQUI_TU_STORAGE_BUCKET",
-  messagingSenderId: "AQUI_TU_SENDER_ID",
-  appId: "AQUI_TU_APP_ID"
+  apiKey: "AIzaSyBZ8lVJl9prE_MWXU5WokKVCwvbjUFvEbQ",
+  authDomain: "fenixzone-roleplay-sv1.firebaseapp.com",
+  projectId: "fenixzone-roleplay-sv1",
+  storageBucket: "fenixzone-roleplay-sv1.firebasestorage.app",
+  messagingSenderId: "410550508472",
+  appId: "1:410550508472:web:252122eda3aadfc035cd4d"
 };
 
 initializeApp(firebaseConfig);
